9c31733c2fcc41c9e60487833bdc2452991dbeec
양선미 <czaeopd@gmail.com>
2025-09-15 20:31:42 +0900

    Update index.js

--- sh02/static/index.js
+++ sh02/static/index.js
@@ -1,1 +1,19 @@
 
+send = document.getElementById("send");
+plus = document.getElementById("plus");
+
+n = 0
+
+send.addEventListener("click", function() {
+  location.href='http://10.150.0.243:5000/save'+n;
+  n = 0;
+  num.innerHTML = n;
+});
+
+plus.addEventListener("click", function() {
+  n+=1;
+});
+
+setInterval(() => {
+  document.querySelector("h2").innerHTML=n;  
+}, 100);
