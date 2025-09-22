46573df834ea11221b51b6c1265d4d03d36a2f0d
양선미 <czaeopd@gmail.com>
2025-09-22 19:12:08 +0900

    Update index.js

--- ch03/static/index.js
+++ ch03/static/index.js
@@ -1,1 +1,12 @@
+const on = document.getElementById("ON");
+const off = document.getElementById("OFF");
 
+function ON_click() {
+    on.style.opacity = 1;
+    off.style.opacity = 0;
+}
+
+function OFF_click() {
+    on.style.opacity = 0;
+    off.style.opacity = 1;
+}
